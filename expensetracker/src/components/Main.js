9f6f25739b64d0e9f66178f66eec8601b802095d
aleksacekic <acanb01@gmail.com>
2024-01-30 19:07:32 +0100

    c

--- expensetracker/src/components/Main.js
+++ expensetracker/src/components/Main.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      
+    <div className="mejn">
+      <div className="balances">
+        <div className="totalbalance">
+          TOTAL BALANCE <div>147 EURO</div>
+        </div>
+        <div className="expenseincome">
+          <div className="expense">EXPENSE</div>
+          <div className="income">INCOME</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
