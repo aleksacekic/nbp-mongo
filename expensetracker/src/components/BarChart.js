fe1a27e02cce95db85b6d1b23ba31a74baf3d165
aleksacekic <acanb01@gmail.com>
2024-02-02 20:05:41 +0100

    final chart

--- expensetracker/src/components/BarChart.js
+++ expensetracker/src/components/BarChart.js
@@ -1,71 +1,100 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Bar } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
 import Chart from "chart.js/auto";
 
 const BarChart = () => {
-  const [chart, setChart] = useState(null);
-  const chartRef = useRef(null);
+  const [selectedData, setSelectedData] = useState("expenses");
 
   useEffect(() => {
-    const data = {
-      labels: ["Food", "Transportation", "Shopping", "Fun", "Health", "Other"],
-      values: [50, 30, 70, 40, 60, 20],
-      label: "Expenses",
-    };
+    let chart;
 
-    if (chart) {
-      chart.destroy();
-    }
+    const fetchDataAndCreateChart = () => {
+      if (chart) {
+        // Uništavanje prethodnog grafikona ako postoji
+        chart.destroy();
+      }
 
-    const ctx = chartRef.current.getContext("2d");
+      const ctx = document.getElementById("myChart").getContext("2d");
 
-    const colors = [
-      "rgba(255, 99, 132, 0.6)",
-      "rgba(54, 162, 235, 0.6)",
-      "rgba(255, 206, 86, 0.6)",
-      "rgba(75, 192, 192, 0.6)",
-      "rgba(153, 102, 255, 0.6)",
-      "rgba(255, 159, 64, 0.6)",
-    ];
+      const data = {
+        labels:
+          selectedData === "expenses"
+            ? ["Food", "Transportation", "Shopping", "Fun", "Health", "Other"]
+            : ["Salary", "Loan", "Gift", "Gambling"],
+        expensesValues: [50, 30, 70, 40, 60, 20],
+        incomesValues: [80, 20, 40, 60],
+        label: selectedData === "expenses" ? "Expenses" : "Incomes",
+      };
 
-    const datasets = data.labels.map((label, index) => ({
-      label,
-      data: [data.values[index]],
-      backgroundColor: colors[index],
-      borderColor: colors[index],
-      borderWidth: 1,
-    }));
+      const values =
+        selectedData === "expenses" ? data.expensesValues : data.incomesValues;
 
-    const newChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: [""],
-        datasets,
-      },
-      options: {
-        scales: {
-          x: {
-            beginAtZero: true,
-          },
-          y: {
-            beginAtZero: true,
+      const colors = [
+        "rgba(255, 99, 132, 0.6)",
+        "rgba(54, 162, 235, 0.6)",
+        "rgba(255, 206, 86, 0.6)",
+        "rgba(75, 192, 192, 0.6)",
+        "rgba(153, 102, 255, 0.6)",
+        "rgba(255, 159, 64, 0.6)",
+      ];
+
+      const datasets = data.labels.map((label, index) => ({
+        label,
+        data: [values[index]],
+        backgroundColor: colors[index],
+        borderColor: colors[index],
+        borderWidth: 1,
+      }));
+
+      chart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: [""],
+          datasets,
+        },
+        options: {
+          scales: {
+            x: {
+              beginAtZero: true,
+            },
+            y: {
+              beginAtZero: true,
+            },
           },
         },
-      },
-    });
+      });
+    };
 
-    setChart(newChart);
+    fetchDataAndCreateChart();
 
+    // Čišćenje grafikona kad se komponenta unmount-uje
     return () => {
-      if (newChart) {
-        newChart.destroy();
+      if (chart) {
+        chart.destroy();
       }
     };
-  }, []);
+  }, [selectedData]);
 
   return (
     <div className="barchart">
-      <canvas ref={chartRef} width="400" height="400"></canvas>
+      <div>
+        <input
+          type="radio"
+          value="expenses"
+          checked={selectedData === "expenses"}
+          onChange={() => setSelectedData("expenses")}
+        />
+        <label>Expenses</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          value="incomes"
+          checked={selectedData === "incomes"}
+          onChange={() => setSelectedData("incomes")}
+        />
+        <label>Incomes</label>
+      </div>
+      <canvas id="myChart" width="400" height="400"></canvas>
     </div>
   );
 };
